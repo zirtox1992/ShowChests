167e89c5f3ea6df13c30daf5efc00d97da160cfb
zirtox1992 <50052382+zirtox1992@users.noreply.github.com>
2022-07-26 14:28:23 +0200

    Delete ShowChests directory

--- ShowChests/core.lua
+++
@@ -1,7 +0,0 @@
-local frame=CreateFrame("Frame");
-frame:RegisterEvent("VARIABLES_LOADED");
-frame:SetScript("OnEvent",function(self,event,...)
-    if (IsAddOnLoaded("pfQuest")) then
-        DEFAULT_CHAT_FRAME.editBox:SetText("/db chests") ChatEdit_SendText(DEFAULT_CHAT_FRAME.editBox, 0)
-    end
-end);
